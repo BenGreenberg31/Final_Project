bb1910986016ff7bb6ac2c8ed95169da168745f0
kathion3 <129798383+kathion3@users.noreply.github.com>
2024-05-14 08:03:46 -0400

    Add files via upload

--- Final_Report/visualization/plots.py
+++ Final_Report/visualization/plots.py
@@ -1,7 +1,10 @@
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from itertools import combinations
+import numpy as np
+import pandas as pd
+# Basic Analysis Questions
 directors_movies_df = pd.read_csv('directors_movies.csv')
 final_credits_df = pd.read_csv('final_credits.csv')
 
@@ -63,3 +66,73 @@ plt.savefig('freq_of_roles_plot.png')
 # plt.xlabel('Array Size')
 # plt.ylabel('Count')
 # plt.legend(loc="lower right")
+
+# Jaccard Similarity Calculations
+from itertools import combinations
+import numpy as np
+import pandas as pd
+#from sklearn.metrics import jaccard_score
+
+final_credits = pd.read_csv('final_credits.csv')
+directors_movies = pd.read_csv('directors_movies.csv')
+
+
+# Map all title_ids to crew
+crew_dict = final_credits.groupby('title_id')['Name'].apply(set).to_dict()
+
+# Jaccard similarity score function from GeeksForGeeks
+def jaccard_similarity(set1, set2):
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union if union != 0 else 0
+
+
+directors = directors_movies.groupby('director_name')
+director_similarity_scores = {}
+
+# Decided to calculate the jaccard similarity for every combination of crews and then average those similarity scores for each director
+
+for director, group in directors:
+    title_ids = group['title_ids'].dropna().unique()
+    if len(title_ids) > 1:
+        scores = []
+        
+        for title_id1, title_id2 in combinations(title_ids, 2):
+            crew1 = crew_dict.get(title_id1, set())
+            crew2 = crew_dict.get(title_id2, set())
+            score = jaccard_similarity(crew1, crew2)
+            scores.append(score)
+
+        director_similarity_scores[director] = np.mean(scores)
+    
+    else:
+        # If a director only has one project set the similarity to 0
+        director_similarity_scores[director] = 0
+
+
+
+sorted_directors = pd.DataFrame(list(director_similarity_scores.items()), columns=['Director', 'Average Jaccard Similarity']).sort_values(by='Average Jaccard Similarity', ascending=False)
+
+#show top directors
+print(sorted_directors.head(10))
+
+descriptive_stats = sorted_directors['Average Jaccard Similarity'].describe()
+
+# plotting directors' jaccard similarity
+plt.figure()
+plt.plot(sorted_directors['Director'], sorted_directors['Average Jaccard Similarity'])
+plt.title('Jaccard Similarity of Directors')
+plt.ylabel('Jaccard Similarity Score')
+plt.xticks([])
+plt.savefig('jaccard_plot.png')
+
+# plotting top ten directors' jaccard similarity
+plt.figure()
+plt.bar(sorted_directors.head(10)['Director'], sorted_directors.head(10)['Average Jaccard Similarity'])
+plt.title('Jaccard Similarity of Directors')
+plt.ylabel('Jaccard Similarity Score')
+plt.xlabel('Director')
+# plt.xticks([])
+plt.xticks(rotation=45, ha='right')
+plt.subplots_adjust(bottom=0.3)
+plt.savefig('jaccard_bar_plot.png')
